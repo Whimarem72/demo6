998a1c6ec09fb2f3dd38521890bb7bb21a085025
DELEC01\alkoyun_r <Resan.Alkoyun@rheinmetall.com>
2017-06-28 09:31:24 +0200

    Demo Ende

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ def main():
 def fortsetzen():
 	print("Die Arbeit wird fortgesetzt.")
 
+def demoEnde()
+	print("Ende der Demo")
+
 if __name__ == "__main__":
 	# execute only if run as a script
 	main()
 	fortsetzen()
+	demoEnde()
